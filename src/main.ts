0995988e3a30808ac26e31bca0b46e68bfa18829
Runyao <hnfanrunyao@gmail.com>
2023-02-26 16:09:22 -0600

    add new review pop up window

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,19 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import { aliases, md } from 'vuetify/iconsets/md'
 
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'md',
+    aliases,
+    sets: {
+      md,
+    },
+  },
 })
 
 createApp(App).use(vuetify).use(store).use(router).mount('#app')
